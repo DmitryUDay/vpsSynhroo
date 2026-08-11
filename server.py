ac05b6d3f69bc53bcdbb502145f67a6769bdbaad
Дмитрий <147247493+DmitryUDay@users.noreply.github.com>
2026-08-11 15:29:57 +0300

    Add files via upload

---
+++ server.py
@@ -0,0 +1,13 @@
+from flask import Flask, jsonify
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
+
+@app.route('/ping', methods=['POST', 'GET'])
+def ping():
+    print(">>> ПРИВЕТ МИР! СИГНАЛ С САЙТА ПОЛУЧЕН! <<<", flush=True)
+    return jsonify({"status": "ok", "message": "Привет с сервера!"})
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8000)
